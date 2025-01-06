81c9b549de7143b852e8230aa8b24fdcd6fffbb0
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2025-01-06 10:55:49 +0200

    feat: remove start scritp

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 ZELLIJ_HOME="$(which zellij)"
+ZELLIJ_CONFIG="$HOME/.config/zellij/config.kdl"
 
 if [ -z $ZELLIJ_HOME ]; then
   ZELLIJ_HOME="$HOME/.cargo/bin/zellij"
 fi
 
-$ZELLIJ_HOME attach --index 0 --create; zsh
+$ZELLIJ_HOME --config $ZELLIJ_CONFIG attach --index 0 --create; zsh
